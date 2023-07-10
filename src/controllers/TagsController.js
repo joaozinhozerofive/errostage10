e9ae72eb1c84b2f70decbb3c6dec2a3555575ea7
joaozinhozerofive <124640745+joaozinhozerofive@users.noreply.github.com>
2023-07-10 20:30:31 -0300

    Add files via upload

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,16 @@
+const knex = require("../database/knex")
+
+
+class Tagscontroller{
+    async index(request, response) {
+        const {user_id} = request.params;
+
+        const tags = await knex("tags")
+        .where({user_id})
+
+        return response.json(tags)
+
+    }
+}
+
+module.exports = Tagscontroller;
